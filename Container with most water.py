39be31631a7ab1ab23f18d18c45bf8adb1fabede
Syed Farman Ali <88277123+FARMAN9@users.noreply.github.com>
2024-04-19 18:57:38 +0530

    Update Container with most water.py

--- Container with most water.py
+++ Container with most water.py
@@ -1,4 +1,4 @@
-def max_area(array):
+def max_area(array): #O(n^2)
     area=0
     length = len(array)
     if length==0 or length==1:
@@ -15,3 +15,32 @@ def max_area(array):
 
 
 print("Max area",max_area([1,2,1]))
+
+
+
+fast O(n)
+from typing import List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        i=0
+        j=len(height)-1
+        max_area=0
+        while i<j:
+            h=min(height[i],height[j])
+            w= j-i
+            max_area=max(max_area,h*w)
+            if height[i]<height[j]:
+                i+=1
+            else:
+                j=j-1
+                    
+
+        return max_area
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    height = [1, 2, 3, 4, 5, 6, 7, 25, 26, 27, 28, 29, 30, 31, 32, 33]
+    result = solution.maxArea(height)
+    print(result)        
