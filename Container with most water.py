ce685d9e7ab2422e83191a7b21e9597cd1dbfac7
Syed Farman Ali <88277123+FARMAN9@users.noreply.github.com>
2024-08-11 21:43:16 +0530

    Update Container with most water.py

--- Container with most water.py
+++ Container with most water.py
@@ -43,4 +43,5 @@ if __name__ == "__main__":
     solution = Solution()
     height = [1, 2, 3, 4, 5, 6, 7, 25, 26, 27, 28, 29, 30, 31, 32, 33]
     result = solution.maxArea(height)
-    print(result)        
+    print(result)  
+    #update
